a308263069d3d625802f6612c3278d9e71db00c8
Dan Pasanen <dan.pasanen@gmail.com>
2017-07-14 05:41:02 -0500

    g2: use lzma for recovery ramdisk
    * running out of space...
    
    Change-Id: I820c0613973cde35c8533a595c4ff81e93166ed8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -38,6 +38,7 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=g2
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x05000000 --tags_offset 0x00000100
+LZMA_RAMDISK_TARGETS := recovery
 TARGET_KERNEL_SOURCE := kernel/lge/msm8974
 
 # Audio
